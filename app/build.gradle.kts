0fa230f413601382b91267145d49965c26594768
Rafael Ruiz <rafael@57blocks.com>
2023-11-10 11:51:45 -0500

    feat: Moved some application Gradle logic to the plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,50 +11,10 @@ android {
         applicationId = "com.example.depsmanagement"
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
 }
 
 dependencies {
-
-    implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.activity.compose)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.ui)
-    implementation(libs.ui.graphics)
-    implementation(libs.ui.tooling.preview)
-    implementation(libs.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    androidTestImplementation(platform(libs.compose.bom))
-    androidTestImplementation(libs.ui.test.junit4)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
-
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.navigation.compose)
-
     implementation(project(":feature:first:domain"))
     implementation(project(":feature:first:infrastructure"))
     implementation(project(":feature:first:view"))
--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -16,10 +16,13 @@
 
 import com.android.build.api.dsl.ApplicationExtension
 import com.example.depsmanagement.convention.ProjectConfig
+import com.example.depsmanagement.convention.configureBuildTypes
 import com.example.depsmanagement.convention.configureKotlinAndroid
+import com.example.depsmanagement.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -31,7 +34,44 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
 
             extensions.configure<ApplicationExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = ProjectConfig.targetSdk
+
+                defaultConfig {
+                    targetSdk = ProjectConfig.targetSdk
+
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    vectorDrawables {
+                        useSupportLibrary = true
+                    }
+                }
+
+                configureBuildTypes(this)
+
+                packaging {
+                    resources {
+                        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+                    }
+                }
+            }
+
+            dependencies {
+                add("implementation", libs.findLibrary("core.ktx").get())
+                add("implementation", libs.findLibrary("lifecycle.runtime.ktx").get())
+                add("implementation", libs.findLibrary("activity.compose").get())
+                add("implementation", platform(libs.findLibrary("compose.bom").get()))
+                add("implementation", libs.findLibrary("ui").get())
+                add("implementation", libs.findLibrary("ui.graphics").get())
+                add("implementation", libs.findLibrary("ui.tooling.preview").get())
+                add("implementation", libs.findLibrary("material3").get())
+                add("testImplementation", libs.findLibrary("junit").get())
+                add("androidTestImplementation", libs.findLibrary("androidx.test.ext.junit").get())
+                add("androidTestImplementation", libs.findLibrary("espresso.core").get())
+                add("androidTestImplementation", platform(libs.findLibrary("compose.bom").get()))
+                add("androidTestImplementation", libs.findLibrary("ui.test.junit4").get())
+                add("debugImplementation", libs.findLibrary("ui.tooling").get())
+                add("debugImplementation", libs.findLibrary("ui.test.manifest").get())
+
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.navigation.compose").get())
             }
         }
     }
